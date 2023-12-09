cc4816b82cee090c6edba4fd1554d14176dfb942
Great & Powerful ShnOZ <pmarchitiello@protonmail.com>
2023-12-08 23:20:39 -0800

    First Assignment 5 commit

---
+++ fancifymytext.js
@@ -0,0 +1,13 @@
+function myAlert(){
+    alert("Hello, world!");
+    document.getElementById("userText").style.fontSize = "24pt";
+}
+
+function myStyle(){
+    alert("FanciyShamncy!");
+    document.getElementById("userText").style.fontWeight = "bold";
+}
+
+function removeStyle(){
+    document.getElementById("userText").style.fontWeight = "normal";
+}
