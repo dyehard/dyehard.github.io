96b01a650066d76387e2d2f6d29c7790d1ee2e0b
Great & Powerful ShnOZ <pmarchitiello@protonmail.com>
2023-12-09 00:22:36 -0800

    Almost done with Assignment 5

--- fancifymytext.js
+++ fancifymytext.js
@@ -6,8 +6,21 @@ function myAlert(){
 function myStyle(){
     alert("FanciyShamncy!");
     document.getElementById("userText").style.fontWeight = "bold";
+    document.getElementById("userText").style.color = "blue";
+    document.getElementById("userText").style.textDecoration = "underline";
 }
 
 function removeStyle(){
     document.getElementById("userText").style.fontWeight = "normal";
+    document.getElementById("userText").style.color = "";
+    document.getElementById("userText").style.textDecoration = "";
+}
+
+function Moo(){
+    document.getElementById("userText").style.textTransform = "uppercase";
+
+    var myText = document.getElementById("userText").textContent;
+    var mySplitText = myText.split(".");
+    myText = mySplitText.join("-Moo");
+    document.getElementById("userText").textContent = "myText";
 }
